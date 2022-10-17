f3c2fc06e8e2d6763662682c43415c664da8c5c2
mje713 <chewyuing@gmail.com>
2022-10-16 21:54:40 -0400

    Add project files.

---
+++ BulldogsTraining/Data/TrainingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulldogsTraining.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulldogsTraining.Data
+{
+    public class TrainingDbContext : DbContext
+    {
+
+        public TrainingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<StudentCreation> studentCreations { get; set; }
+    }
+}
---
+++ BulldogsTraining/Models/Domains/StudentCreation.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace BulldogsTraining.Domains
+{
+    public class StudentCreation
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Airframe { get; set; }
+        public int Scale { get; set; }
+        public string InstructorName { get; set; }
+        public bool Confirm { get; set; }
+    }
+}
